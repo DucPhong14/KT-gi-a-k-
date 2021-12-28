5974539b88893277aba05b795e308b4050227767
DucPhong14 <phonghd123456789@gmail.com>
2021-12-28 20:58:03 +0700

    them

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let bnt_Dong_y = document.getElementById("bnt_Dong_y");
+let bnt_Reset = document.getElementById("bnt_Reset");
+
+let txt_Ten = document.getElementById("txt_Ten");
+let txt_DiaChi = document.getElementById("txt_DiaChi");
+let txt_SDT = document.getElementById("txt_SDT");
+let txt_Ten = document.getElementById("txt_SDT");
+
+bnt_Dong_y.addEventListener("click", function () {});
+bnt_Reset.addEventListener("click", function () {});
